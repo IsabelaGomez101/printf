a173e3cf1bed993fb48a0870ea885a683da7bfd6
IsabelaGomez101 <3379@holbertonschool.com>
2021-07-26 11:06:09 -0500

    header

---
+++ holberton.h
@@ -0,0 +1,22 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * struct print_types- structure that containing two variables
+ * @tp: Type of argument.
+ * @f: The function associated.
+ */
+typedef struct print_types
+{
+	char *tp;
+	void (*f)(va_list arg);
+} pr_tp;
+
+int _printf(const char *format, ...);
+
+#endif
