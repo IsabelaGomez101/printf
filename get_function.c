f3b36b8dc5f69eb31d4622549c5ea0be9d6c28f1
knia10 <kelly.stephania@hotmail.com>
2021-07-27 03:03:27 +0000

    Add file get_function

---
+++ get_function.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * get_function - structure that containing two variables
+ * @format: Type of argument.
+ * Return: pointer.
+ */
+
+
+int (get_function(const char *format))(va_list arg)
+{
+	pr_tp prt[] = {
+		{"c", _pr_char},
+		{"s", _pr_string},
+		{NULL, NULL},
+
+	};
+
+	int j = 0;
+
+	while (prt[j].tp != NULL)
+	{
+		if (format[0] == *(prt[j].tp))
+		{
+			break;
+
+		}
+		j++;
+
+	}
+	return (prt[j].f);
+}
