3af63c80d4598672e076b6bfc7f9c3e29760b0b7
knia10 <kelly.stephania@hotmail.com>
2021-07-27 03:00:37 +0000

    Add file function

---
+++ function.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * _pr_char - function that print a character.
+ * @arg: list arguments.
+ * Return: length.
+ */
+
+int _pr_char(va_list arg)
+{
+	 char c = va_arg(arg, int);
+
+	 _putchar(c);
+return (1);
+}
+
+/**
+ * _pr_string - function that print a string.
+ * @arg: list arguments.
+ * Return: length.
+ */
+
+int _pr_string(va_list arg)
+{
+	int i;
+	char s = va_arg(arg, char);
+
+	for (i = 0; i < _strlen(s); i++)
+	{
+		_putchar(s[i]);
+
+	}
+	return (_strlen(s));
+}
