19a3739efa99222401dd08f8ef4f38c1369a2a93
knia10 <kelly.stephania@hotmail.com>
2021-07-28 19:13:35 +0000

    Add function pr_numbers

--- function.c
+++ function.c
@@ -1,40 +1,73 @@
 #include "holberton.h"
-
 /**
  * _pr_char - function that print a character.
  * @arg: list arguments.
  * Return: length.
  */
-
 int _pr_char(va_list arg)
 {
-	 char c = va_arg(arg, int);
-
-	 _putchar(c);
-return (1);
+	_putchar(va_arg(arg, int));
+	return (1);
 }
-
 /**
  * _pr_string - function that print a string.
  * @arg: list arguments.
  * Return: length.
  */
-
 int _pr_string(va_list arg)
 {
-	int i;
+	int i = 0;
 	char *s = va_arg(arg, char *);
-
-	for (i = 0; i < _strlen(s); i++)
+	if (s == NULL)
+	{
+		s = "null";
+	}
+	while (s[i])
 	{
 		_putchar(s[i]);
-
+		i++;
 	}
-	return (_strlen(s));
+	return (i);
 }
-
-int _pr_percent()
+/**
+ * _pr_percent - function that print a percent.
+ * Return: length.
+ */
+int _pr_percent(void)
 {
 	_putchar(37);
 	return (1);
+}
+/**
+ * _pr_numbers - function that print numbers.
+ * @arg: list the arguments.
+ * Return: length.
+ */
+int _pr_numbers(va_list arg)
+{
+	int num = va_arg(arg, int);
+	int div = 1;
+	int n, digit;
+	int l = 0;
+	if (num < 0)
+	{
+		_putchar('-');
+		num = num * -1;
+		l++;
+	}
+	n = num;
+	while ((num / 10) != 0)
+	{
+		num = num / 10;
+		div *= 10;
+	}
+	while (div >= 1)
+	{
+		digit = n / div;
+		_putchar(digit + '0');
+		n = n % div;
+		div = div / 10;
+		l++;
+	}
+	return (l);
 }
